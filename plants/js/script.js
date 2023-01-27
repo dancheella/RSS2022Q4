14e50445631c7038c807cc3113b47455af5fe2c1
dancheella <ulman33312@gmail.com>
2023-01-27 23:45:01 +0300

    feat: add prices accourdion

--- plants/js/script.js
+++ plants/js/script.js
@@ -1,6 +1,6 @@
+//Burger menu
 const menu = document.querySelector('.menu__body')
 const menuBtn = document.querySelector('.header__burger')
-
 const body = document.body;
 
 if (menu && menuBtn) {
@@ -9,7 +9,6 @@ if (menu && menuBtn) {
         menuBtn.classList.toggle('active')
         body.classList.toggle('lock')
     })
-
     menu.addEventListener('click', e => {
         if (e.target.classList.contains('menu__body')) {
             menu.classList.remove('active')
@@ -17,7 +16,6 @@ if (menu && menuBtn) {
             body.classList.remove('lock')
         }
     })
-
     menu.querySelectorAll('.menu__link').forEach(link => {
         link.addEventListener('click', () => {
             menu.classList.remove('active')
@@ -26,3 +24,39 @@ if (menu && menuBtn) {
         })
     })
 }
+
+//prices accourdion
+const itemsPrices = document.querySelectorAll('.prices__item'); // Выборка всех элементов с классом prices__item из документа.
+const accordion = document.querySelectorAll('.prices__accordion'); // Выборка всех элементов с классом prices__accordion из документа.
+
+accordion.forEach((prices) => {
+    prices.addEventListener('click', () => { // Навешивается событие click.
+        if (prices.classList.contains('active')) { // Проверка, есть ли у текущего элемента класс "active".
+            prices.classList.remove('active'); // Если есть класс "active", то класс удаляется и у родительского элемента тоже удаляется класс"active".
+            prices.parentElement.classList.remove('active');
+        } else {
+            itemsPrices.forEach(i => i.classList.remove('active'));
+            accordion.forEach(i => i.classList.remove('active'));
+            prices.classList.add('active');
+            prices.parentElement.classList.add('active');
+        } //Если же класса "active" нет, то из всех элементов "itemsPrices" и "accordion" удаляется класс "active",
+        // а затем добавляется класс "active" для текущего элемента "accordion" и его родителя.
+    });
+});
+
+//button order
+const basicsLink = document.getElementById("basicsLink");
+const standardLink = document.getElementById("standardLink");
+const proLink = document.getElementById("proLink");
+
+basicsLink.addEventListener("click", function () {
+    window.location.href = "#contacts";
+});
+standardLink.addEventListener("click", function () {
+    window.location.href = "#contacts";
+});
+proLink.addEventListener("click", function () {
+    window.location.href = "#contacts";
+});
+
+
