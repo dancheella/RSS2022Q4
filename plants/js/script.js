3e2d827a63798f598c742965b2ab6a450503766f
dancheella <ulman33312@gmail.com>
2023-02-01 14:14:47 +0300

    fix: adaptive file

--- plants/js/script.js
+++ plants/js/script.js
@@ -5,11 +5,9 @@ const body = document.body;
 
 if (menu && menuBtn) {
     menuBtn.addEventListener('click', e => {
-        if (e.target.classList.contains('menu__body')) {
             menu.classList.toggle('active')
             menuBtn.classList.toggle('active')
             body.classList.toggle('lock')
-        }
     })
     menu.addEventListener('click', e => {
         if (e.target.classList.contains('menu__body')) {
@@ -282,4 +280,3 @@ plantingButton.addEventListener("click", function (e) {
         toggleBlurAllServiceItems();
     }
 });
-
