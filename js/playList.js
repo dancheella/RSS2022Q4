64b0ae7ac083fdf995babdb6c0dc56103b34ac52
Denis Dankin <denisdankin@192.168.100.2>
2023-02-27 00:26:27 +0300

    fix: time song

--- js/playList.js
+++ js/playList.js
@@ -2,22 +2,22 @@ const playList = [
     {
         title: 'KORDHELL - LAND OF FIRE',
         src: '../assets/sounds/KORDHELL - LAND OF FIRE.mp3',
-        duration: '02:33'
+        duration: '2:33'
     },
     {
         title: 'LIUE – SUFFER WITH ME',
         src: '../assets/sounds/LIUE – SUFFER WITH ME.mp3',
-        duration: '01:13'
+        duration: '1:13'
     },
     {
         title: 'INTERWORLD - METAMORPHOSIS',
         src: '../assets/sounds/INTERWORLD - METAMORPhOSIS.mp3',
-        duration: '01:59'
+        duration: '1:59'
     },
     {
         title: 'CYPARISS - EMPTY DREAMS',
         src: '../assets/sounds/CYPARISS - EMPTY DREAMS.mp3',
-        duration: '02:15'
+        duration: '2:15'
     }
 ]
 export default playList;
